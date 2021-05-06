14237223612b6e3dd4523002e665ba77acff789d
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-06 22:47:25 +0530

    Re-exports for contexts

---
+++ src/contexts/index.js
@@ -0,0 +1,11 @@
+export { default as GetProductsProvider } from "./getproducts-context";
+export { default as ProductProvider } from "./products-context";
+export { default as ToastProvider } from "./toast-context";
+export { default as UserDataProvider } from "./userdata-context";
+export { default as AuthProvider } from "./auth.context";
+
+export { useGetProducts } from "./getproducts-context";
+export { useProduct } from "./products-context";
+export { useToast } from "./toast-context";
+export { useUserData } from "./userdata-context";
+export { useAuth } from "./auth.context";
