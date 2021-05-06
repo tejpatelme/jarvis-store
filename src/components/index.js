10d1b52aa78f5859aab239e53ef309cef610cb57
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-06 22:46:45 +0530

    Re-exports file for Components

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as NavBar } from "./NavBar/NavBar";
+export { default as ProductCard } from "./ProductCard/ProductCard";
+export { default as ProductsContainer } from "./ProductsContainer/ProductsContainer";
+export { default as Toast } from "./Toast/Toast";
+export { default as ToastContainer } from "./Toast/ToastContainer";
+export { default as Sidebar } from "./Sidebar/Sidebar";
+export { default as MobileSortAndFilter } from "./MobileSortAndFilter/MobileSortAndFilter";
+export { default as MobileSortMenu } from "./MobileSortAndFilter/MobileSortMenu";
+export { default as MobileFilterMenu } from "./MobileSortAndFilter/MobileFilterMenu";
