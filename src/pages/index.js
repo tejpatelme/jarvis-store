16ece8017616733a600a8609890bf7cfaf10be27
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-09-06 10:21:12 +0530

    Revert "Refactor"

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Home } from "./Home/Home";
-export { default as Cart } from "./Cart/Cart";
-export { default as Wishlist } from "./Wishlist/Wishlist";
-export { default as Products } from "./Products/Products";
-export { default as Login } from "./Login/Login";
-export { default as Signup } from "./Signup/Signup";
-export { default as ProductPage } from "./ProductPage/ProductPage";
