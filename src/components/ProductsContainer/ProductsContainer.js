27bfe917768d16723930a903d5cba3415d1ad87c
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-09-07 13:55:27 +0530

    refactor : remove fragments

--- src/components/ProductsContainer/ProductsContainer.js
+++ src/components/ProductsContainer/ProductsContainer.js
@@ -3,12 +3,10 @@ import { ProductCard } from "../";
 
 export default function ProductsContainer({ filteredProducts }) {
   return (
-    <>
-      <div className="prod-container">
-        {filteredProducts.map((prod) => {
-          return <ProductCard key={prod._id} details={prod} />;
-        })}
-      </div>
-    </>
+    <div className="prod-container">
+      {filteredProducts.map((prod) => {
+        return <ProductCard key={prod._id} details={prod} />;
+      })}
+    </div>
   );
 }
